cafcd99b23f9fccf63678348efb90243809e2e9e
webfarrock <webfarrock@mail.ru>
2021-10-27 09:51:49 +0300

    task09

---
+++ src/task-09/part-01.test.js
@@ -0,0 +1,14 @@
+const isTriangleRight = require('./part-01');
+
+describe('checks task №09 part №01', () => {
+    it("checks is triangle right", () => {
+        [
+            [3, 4, 5, true],
+            [5, 4, 3, true],
+            [0, 10, 20, false],
+            [-1, 0, 70, false],
+        ].forEach(([a, b, c, expectedResult]) => {
+            expect(isTriangleRight(a, b, c)).toBe(expectedResult);
+        });
+    });
+});
---
+++ src/task-09/part-02.test.js
@@ -0,0 +1,44 @@
+const {outputCircleLengthAndArea, INPUT_ERROR_MESSAGE} = require('./part-02');
+
+describe('checks task №09 part №02', () => {
+    const log = console.log;
+
+    beforeEach(() => {
+        console.log = jest.fn();
+    });
+
+    afterEach(() => {
+        console.log = log;
+    });
+
+    it("checks correct input", () => {
+        [
+            ['0', 'length: 0, area: 0'],
+            ['5', 'length: 31.41592653589793, area: 19.634954084936208'],
+            ['10', 'length: 62.83185307179586, area: 78.53981633974483'],
+            ['17', 'length: 106.81415022205297, area: 226.98006922186255'],
+            ['24', 'length: 150.79644737231007, area: 452.3893421169302'],
+        ].forEach(([input, result]) => {
+            jest.spyOn(window, "prompt").mockImplementation(() => input);
+            outputCircleLengthAndArea()
+            expect(console.log).toHaveBeenCalledWith(result);
+        });
+    });
+
+    it("checks wrong input", () => {
+        [
+            "",
+            "27 11",
+            "one",
+            "#$^",
+            "-1",
+            "-78",
+        ].forEach((input) => {
+            jest.spyOn(window, "prompt").mockImplementation(() => input);
+
+            expect(() => {
+                outputCircleLengthAndArea()
+            }).toThrow(INPUT_ERROR_MESSAGE);
+        });
+    });
+});
