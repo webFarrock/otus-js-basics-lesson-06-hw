242b4c86010759c629a56e543d38748a8870c0e3
webfarrock <webfarrock@mail.ru>
2021-10-30 13:48:24 +0300

    try to break tests

--- src/task-01/part-01.js
+++ src/task-01/part-01.js
@@ -1,9 +1,9 @@
 function sumOutput(a, b) {
-  console.log(a + b);
+  console.log(a + b + 1);
 }
 
 function multiplyOutput(a, b) {
-  console.log(a * b);
+  console.log(a * b * 2);
 }
 
 export { sumOutput, multiplyOutput };
